90a601bfa8aa87c69b75e57317b2cb671f0ed7bd
Kapil <Kapilgargymca@gmail.com>
2024-05-30 01:23:57 +0530

    ist commit

---
+++ Sanjeevan-booti-system/src/main/java/com/example/Sanjeevanbootisystem/SanjeevanBootiSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.Sanjeevanbootisystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanjeevanBootiSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanjeevanBootiSystemApplication.class, args);
+		//System.out.print("project start");
+	}
+
+}
---
+++ Sanjeevan-booti-system/src/test/java/com/example/Sanjeevanbootisystem/SanjeevanBootiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sanjeevanbootisystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanjeevanBootiSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
